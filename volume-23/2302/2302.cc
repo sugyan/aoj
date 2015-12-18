f48c4bedb774323b4ebfb12d694e8264903c0212
sugyan <sugi1982@gmail.com>
2015-12-19 00:54:06 +0900

    update 2302.cc

--- volume-23/2302/2302.cc
+++ volume-23/2302/2302.cc
@@ -1,77 +1,58 @@
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
 
 typedef pair<int, int> P;
-typedef pair<P, P> PP;
-typedef vector<P> V;
-typedef map<PP, V> Cache;
 const int MAX_SIZE = 50;
 const int MAX_TASK_SIZE = 1000;
 
-vector<P> dfs(char f[MAX_SIZE][MAX_SIZE], P src, P des, int R, int C) {
-  vector<P> v;
+int R, C, M, a = 0;
+int p[MAX_SIZE][MAX_SIZE] = {};
+char f[MAX_SIZE][MAX_SIZE];
+int costs[3][MAX_SIZE][MAX_SIZE];
+
+int move(P src, P des, int t) {
   if (src == des) {
-    v.push_back(des);
-    return v;
+    return t;
   }
   f[src.first][src.second] = '@';
+  int ret = 0;
   int dr[] = {-1, 1, 0, 0};
   int dc[] = {0, 0, -1, 1};
   for (int k = 0; k < 4; ++k) {
     int i = src.first  + dr[k];
     int j = src.second + dc[k];
     if (i + 1 && j + 1 && i < R && j < C && f[i][j] == '.') {
-      v = dfs(f, P(i, j), des, R, C);
-      if (v.size()) {
-        v.insert(v.begin(), src);
+      ret = move(P(i, j), des, t + 1);
+      if (ret) {
+        int cost = costs[1][i][j] + costs[2][i][j];
+        a += cost;
+        if (p[i][j]) {
+          int d = (t - p[i][j]) * costs[0][i][j] - cost;
+          if (d < 0) a += d;
+        }
+        p[i][j] = t;
         break;
       }
     }
   }
   f[src.first][src.second] = '.';
-  return v;
+  return ret;
 }
 
-int calc(int R, int C, int M, char f[][MAX_SIZE], int costs[][MAX_SIZE][MAX_SIZE], P tasks[]) {
-  Cache cache;
-  int p[MAX_SIZE][MAX_SIZE] = {};
-  int t = 0;
+int calc(P tasks[]) {
+  int t = 1;
   int i0 = tasks[0].first, j0 = tasks[0].second;
-  int a = costs[1][i0][j0] + costs[2][i0][j0];
-  p[i0][j0] = ++t;
+  a = costs[1][i0][j0] + costs[2][i0][j0];
+  p[i0][j0] = t++;
   for (int i = 0; i < M - 1; ++i) {
-    P src = tasks[i], des = tasks[i + 1];
-    V v;
-    Cache::iterator find = cache.find(PP(src, des));
-    if (find != cache.end()) {
-      v = find->second;
-    } else {
-      v = dfs(f, src, des, R, C);
-      cache.insert(pair<PP, V>(PP(src, des), v));
-    }
-    for (V::iterator vit = ++v.begin(); vit != v.end(); ++vit) {
-      ++t;
-      int r = vit->first, c = vit->second;
-      int prev = p[r][c];
-      int cost = costs[1][r][c] + costs[2][r][c];
-      a += cost;
-      if (prev) {
-        int d = (t - prev) * costs[0][r][c] - cost;
-        if (d < 0) a += d;
-      }
-      p[r][c] = t;
-    }
+    t = move(tasks[i], tasks[i + 1], t);
   }
   return a;
 }
 
 int main() {
   int n;
-  int R, C, M;
-  char f[MAX_SIZE][MAX_SIZE];
-  int costs[3][MAX_SIZE][MAX_SIZE];
   P tasks[MAX_TASK_SIZE];
   char s[MAX_SIZE + 1];
   cin >> R >> C >> M;
@@ -94,5 +75,5 @@ int main() {
     cin >> r >> c;
     tasks[i] = P(r, c);
   }
-  cout << calc(R, C, M, f, costs, tasks) << endl;
+  cout << calc(tasks) << endl;
 }
